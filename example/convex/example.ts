6edf68286fb70dea713a18b0b837efe1cd2e2fe4
Ian Macartney <ianhmacartney@gmail.com>
2025-07-31 14:42:23 -0700

    simple example without api key

--- example/convex/example.ts
+++ example/convex/example.ts
@@ -3,184 +3,125 @@ import {
   query,
   action,
   internalMutation,
-  internalQuery,
   internalAction,
   mutation,
 } from "./_generated/server";
 import { internal, components } from "./_generated/api";
-import { CUISINES, EXAMPLE_DATA } from "./constants";
-import { ActionCache } from "@convex-dev/action-cache";
+import { ActionCache, removeAll } from "@convex-dev/action-cache";
+import schema from "./schema";
 
-const embeddingsCache = new ActionCache(components.actionCache, {
-  action: internal.example.embed,
-  name: "embed-v1",
+const geocodingCache = new ActionCache(components.actionCache, {
+  action: internal.example.geocode,
+  name: "geocode",
+  ttl: 1000 * 60 * 60 * 24 * 7, // 1 week: locations don't change
 });
 
-export const embed = internalAction({
-  args: { text: v.string() },
-  handler: async (_ctx, { text }): Promise<number[]> => {
-    const apiKey = process.env.OPENAI_API_KEY;
-    if (!apiKey) {
-      throw new Error("OPENAI_API_KEY environment variable not set!");
+const weatherCache = new ActionCache(components.actionCache, {
+  action: internal.example.getWeather,
+  name: "weather",
+  ttl: 1000 * 60 * 5, // 5 minutes: weather can change quickly!
+});
+
+type Weather = {
+  updated: string;
+  temperature: number;
+  feelsLike: number;
+};
+
+export const geocode = internalAction({
+  args: { location: v.string() },
+  handler: async (_ctx, { location }): Promise<[lat: number, long: number]> => {
+    const geocodingUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(location)}&count=1`;
+    const geocodingResponse = await fetch(geocodingUrl);
+    const geocodingData = (await geocodingResponse.json()) as {
+      results: {
+        latitude: number;
+        longitude: number;
+        name: string;
+      }[];
+    };
+    const result = geocodingData.results[0];
+    if (!result) {
+      throw new Error(`No geocoding result found for ${location}`);
     }
-    const req = { input: text, model: "text-embedding-ada-002" };
-    const resp = await fetch("https://api.openai.com/v1/embeddings", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify(req),
-    });
-    if (!resp.ok) {
-      const msg = await resp.text();
-      throw new Error(`OpenAI API error: ${msg}`);
-    }
-    const json = await resp.json();
-    const vector = json["data"][0]["embedding"] as number[];
-    console.log(`Computed embedding of "${text}": ${vector.length} dimensions`);
-    return vector;
+    console.log("geocoded location", location, result);
+    return [result.latitude, result.longitude];
+  },
+});
+
+export const getWeather = internalAction({
+  args: { latitude: v.number(), longitude: v.number() },
+  handler: async (_ctx, { latitude, longitude }): Promise<Weather> => {
+    const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m,wind_gusts_10m,weather_code&wind_speed_unit=mph&temperature_unit=fahrenheit`;
+
+    const response = await fetch(weatherUrl);
+    const data = (await response.json()) as {
+      current: {
+        time: string;
+        temperature_2m: number;
+        apparent_temperature: number;
+      };
+    };
+    console.log("got weather for location", data.current);
+    return {
+      updated: data.current.time,
+      temperature: Number(data.current.temperature_2m),
+      feelsLike: Number(data.current.apparent_temperature),
+    };
+  },
+});
+
+export const getWeatherForLocation = action({
+  args: { location: v.optional(v.string()), force: v.optional(v.boolean()) },
+  handler: async (ctx, args): Promise<Weather> => {
+    const location = args.location ?? "New York";
+    const start = Date.now();
+    const [lat, long] = await geocodingCache.fetch(
+      ctx,
+      { location },
+      { force: args.force }
+    );
+    const weather = await weatherCache.fetch(
+      ctx,
+      { latitude: lat, longitude: long },
+      { force: args.force }
+    );
+    const end = Date.now();
+    console.log(`getWeatherForLocation(${location}) took ${end - start}ms`);
+    return weather;
   },
 });
 
 export const populate = action({
   args: {},
   handler: async (ctx) => {
-    for (const doc of EXAMPLE_DATA) {
-      const embedding = await embeddingsCache.fetch(ctx, {
-        text: doc.description,
+    for (const location of ["New York", "San Francisco", "Tokyo"]) {
+      const [lat, long] = await geocodingCache.fetch(ctx, {
+        location,
       });
-      await ctx.runMutation(internal.example.insertRow, {
-        cuisine: doc.cuisine,
-        description: doc.description,
-        embedding,
-      });
-    }
-  },
-});
-
-export const insert = action({
-  args: { cuisine: v.string(), description: v.string() },
-  handler: async (ctx, args) => {
-    const embedding = await embeddingsCache.fetch(ctx, {
-      text: args.description,
-    });
-    const doc = {
-      cuisine: args.cuisine,
-      description: args.description,
-      embedding,
-    };
-    await ctx.runMutation(internal.example.insertRow, doc);
-  },
-});
-
-export const insertRow = internalMutation({
-  args: {
-    description: v.string(),
-    cuisine: v.string(),
-    embedding: v.array(v.float64()),
-  },
-  handler: async (ctx, args) => {
-    if (!Object.prototype.hasOwnProperty.call(CUISINES, args.cuisine)) {
-      throw new Error(`Invalid cuisine: ${args.cuisine}`);
-    }
-    await ctx.db.insert("foods", args);
-  },
-});
-
-export const list = query(async (ctx) => {
-  const docs = await ctx.db.query("foods").order("desc").take(10);
-  return docs.map((doc) => {
-    return { _id: doc._id, description: doc.description, cuisine: doc.cuisine };
-  });
-});
-
-export const fetchResults = internalQuery({
-  args: {
-    results: v.array(v.object({ _id: v.id("foods"), _score: v.float64() })),
-  },
-  handler: async (ctx, args) => {
-    const out: SearchResult[] = [];
-    for (const result of args.results) {
-      const doc = await ctx.db.get(result._id);
-      if (!doc) {
-        continue;
-      }
-      out.push({
-        _id: doc._id,
-        description: doc.description,
-        cuisine: doc.cuisine,
-        _score: result._score,
+      await weatherCache.fetch(ctx, {
+        latitude: lat,
+        longitude: long,
       });
     }
-    return out;
-  },
-});
-
-export const vectorSearch = action({
-  args: { query: v.string(), cuisines: v.optional(v.array(v.string())) },
-  handler: async (ctx, args) => {
-    const embedding = await embeddingsCache.fetch(ctx, {
-      text: args.query,
-    });
-    let results;
-    const cuisines = args.cuisines;
-    if (cuisines !== undefined) {
-      results = await ctx.vectorSearch("foods", "by_embedding", {
-        vector: embedding,
-        limit: 16,
-        filter: (q) =>
-          q.or(...cuisines.map((cuisine) => q.eq("cuisine", cuisine))),
-      });
-    } else {
-      results = await ctx.vectorSearch("foods", "by_embedding", {
-        vector: embedding,
-        limit: 16,
-      });
-    }
-    const rows: SearchResult[] = await ctx.runQuery(
-      internal.example.fetchResults,
-      { results },
-    );
-    return rows;
-  },
-});
-
-export type SearchResult = {
-  _id: string;
-  description: string;
-  cuisine: string;
-  _score: number;
-};
-
-export const test = action({
-  args: {},
-  handler: async (ctx) => {
-    const embedding = await embeddingsCache.fetch(ctx, {
-      text: "test",
-    });
-    if (embedding.length !== 1536) {
-      throw new Error(`Expected 1536 dimensions, got ${embedding.length}`);
-    }
-    console.log("Got embedding!");
   },
 });
 
 export const testConcurrently = action({
   args: {
-    text: v.string(),
-    count: v.number(),
+    places: v.array(v.string()),
+    clear: v.optional(v.boolean()),
   },
-  handler: async (ctx, args) => {
+  handler: async (ctx, { places, clear }) => {
+    if (clear) {
+      await geocodingCache.removeAll(ctx);
+    }
     const start = Date.now();
     const promises = [];
-    for (let i = 0; i < args.count; i++) {
+    for (const location of places) {
       const promise = async () => {
         const start = Date.now();
-        const embedding = await embeddingsCache.fetch(ctx, { text: args.text });
-        if (embedding.length !== 1536) {
-          throw new Error(`Expected 1536 dimensions, got ${embedding.length}`);
-        }
+        await geocodingCache.fetch(ctx, { location });
         const end = Date.now();
         return end - start;
       };
@@ -188,7 +129,9 @@ export const testConcurrently = action({
     }
     const individualDurations = await Promise.all(promises);
     const totalDuration = Date.now() - start;
-    console.log(`Loaded ${args.count} embeddings in ${totalDuration}ms`);
+    console.log(
+      `Loaded weather for ${places.length} places in ${totalDuration}ms`
+    );
     for (const individualDuration of individualDurations) {
       console.log(`  Fetch: ${individualDuration}ms`);
     }
@@ -199,10 +142,10 @@ export const clear = mutation({
   args: {},
   handler: async (ctx) => {
     // Remove one entry by arguments.
-    await embeddingsCache.remove(ctx, { text: "test" });
+    await geocodingCache.remove(ctx, { location: "New York" });
     // Remove all entries for this function.
-    await embeddingsCache.removeAllForName(ctx);
+    await geocodingCache.removeAllForName(ctx);
     // Remove all entries for all functions the component.
-    await embeddingsCache.removeAll(ctx);
+    await removeAll(ctx, components.actionCache);
   },
 });
